6e2bbd8a6f56cc07be4d560a0dc1874c34531387
Abdiel Francisco <juanabdiel56@gmail.com>
2025-03-24 20:44:26 -0400

    images update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,9 +9,11 @@ import { ArticlesComponent } from '../articles/articles.component';
 })
 export class HomeComponent implements AfterViewInit {
   //
-  imgBgLoginUrl: string = 'images/fondos/bg.jpg'; // Ruta de la imagen
-  imgIconAngular: string = 'icons/favicon.ico';
-  imgIconGoogle: string = 'icons/google_icon.ico';
+  images = {
+    bgLogin: 'images/fondos/bg.jpg',
+    angularIcon: 'icons/favicon.ico',
+    googleIcon: 'icons/google_icon.ico'
+  };
 
   // Usa el operador '!' para indicar a TypeScript que la propiedad será asignada más tarde
   @ViewChild(ArticlesComponent) articlesComponent!: ArticlesComponent;
